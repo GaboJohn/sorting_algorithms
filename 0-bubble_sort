83978b103b978b717d31064e9abbb4d23175ee89
johngabotsalelwe@gmail.com <johngabotsalelwe@gmail.com>
2023-12-15 05:45:36 -0800

    Bubble sort

---
+++ 0-bubble_sort
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two integers
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using Bubble Sort
+ * @array: Pointer to the array to be sorted
+ * @size: Size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len, sort;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (len = 0; len < size - i - 1; len++)
+		{
+			if (array[len] > array[len + 1])
+		{
+			swap(&array[len], &array[len + 1]);
+
+			for (sort = 0; sort < size; sort++)
+			{
+
+				printf("%d ", array[sort]);
+			}
+			printf("\n");
+		}
+		}
+	}
+}
--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,40 +1,46 @@
 #include "sort.h"
 
 /**
- * swap - Swaps two integers
- * @a: Pointer to the first integer
- * @b: Pointer to the second integer
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
-void swap(int *a, int *b)
+void swap_ints(int *a, int *b)
 {
-	int temp = *a;
+	int tmp;
+
+	tmp = *a;
 	*a = *b;
-	*b = temp;
+	*b = tmp;
 }
+
 /**
- * bubble_sort - Sorts an array of integers in ascending order using Bubble Sort
- * @array: Pointer to the array to be sorted
- * @size: Size of the array
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, len, sort;
+	size_t i, len = size;
+	bool bubbly = false;
 
-	for (i = 0; i < size - 1; i++)
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
 	{
-		for (len = 0; len < size - i - 1; len++)
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
 		{
-			if (array[len] > array[len + 1])
-		{
-			swap(&array[len], &array[len + 1]);
-
-			for (sort = 0; sort < size; sort++)
+			if (array[i] > array[i + 1])
 			{
-
-				printf("%d ", array[sort]);
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
 			}
-			printf("\n");
 		}
-		}
+		len--;
 	}
 }
